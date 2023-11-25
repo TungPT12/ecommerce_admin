266ad8e26ae11bcafef4f72c9b662a1a803e0dfe
TungPT12 <mylifeundermycontrol@gmail.com>
2023-11-25 16:53:25 +0700

    set up socket

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
-
+import { io } from 'socket.io-client';
 function App() {
+  console.log(process.env.REACT_APP_API_LOCALHOST_ENDPOINT)
+  console.log(process.env.REACT_APP_API_LOCALHOST_ENDPOINT)
+  console.log(process.env)
+
+  const socket = io('http://localhost:5000')
+
+
   return (
     <div className="App">
     </div>
