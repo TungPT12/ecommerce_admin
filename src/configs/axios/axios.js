500b869bf28bd5f93aa6b1f640b10262a9ddd203
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-14 08:48:02 +0700

    test cookie on production

--- src/configs/axios/axios.js
+++ src/configs/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000/api/',
-    // baseURL: 'https://booking-server-gafr.onrender.com/api/',
+    // baseURL: 'https://tungstore.onrender.com/api/',
     withCredentials: true
 });
 
