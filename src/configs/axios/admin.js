0a3d203ad15a4f9f6f39199b71eba6a9335b2a15
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-14 18:21:06 +0700

    delete detail button in dashboard

--- src/configs/axios/admin.js
+++ src/configs/axios/admin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosAdminInstance = axios.create({
-    // baseURL: 'http://localhost:5000/api/admin',
-    baseURL: 'https://tungstore.onrender.com/api/admin',
+    baseURL: 'http://localhost:5000/api/admin',
+    // baseURL: 'https://tungstore.onrender.com/api/admin',
     withCredentials: true
 });
 
